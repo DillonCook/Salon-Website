a9a99306758cefcbc529df11bc04b05c8efd59bc
Dillon Cook <dillonlee.cook@gmail.com>
2019-07-18 17:24:26 -0400

    styled menu icon

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Scroll down on arrow down click
+// Scroll down to next section on arrow click
 $('.fa-angle-double-down').click(() => {
     $('html, body').animate({
         scrollTop: $('.gallery').offset().top
